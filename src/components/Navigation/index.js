0a6217df6d93cdedf29cc827ac88ee7233e31481
Matthew Sievers <mattersievers@gmail.com>
2021-11-05 16:28:19 -0500

    added component file structure

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <section>
+            <h1>Navigation</h1>
+        </section>
+    )
+}
+
+export default Navigation;
---
+++ src/components/Project/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Project = () => {
+    return (
+        <section>
+            <h1>Project</h1>
+        </section>
+    )
+}
+
+export default Project;
