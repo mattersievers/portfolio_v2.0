3aa08a03fa52aa6af46fb95850134a1ed2d2b4ab
Matthew Sievers <mattersievers@gmail.com>
2021-11-07 01:27:50 -0600

    fixed css spacing with about me font

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,11 +4,12 @@ const AboutMe = () => {
     return(
         <section className="aboutMe d-flex flex-column justify-content-around">
             <img className="align-self-center" alt="A very studious Matthew" src={headshot}/>
-            <p className="align-self-center">
+            <div className="align-self-center">
+            <p className="">
                 I am a web developer that lives in Austin, Texas. I have experience with HTML, CSS, JavaScript, and Python. I obtained my masters in Mathematics from Texas State University in the Spring of 2017 and started teaching at Austin Community College shortly after.
                 Like many people that live in the music capital, one of my favorite pastime hobbies includes playing music. I play several instruments including drums, bass, guitar, and trombone. I also have some experience with recording and production.
             </p>
-
+            </div>
         </section>
     )
 }
