9b4a3191c429e26df6f85c0db71e32c021332909
Matthew Sievers <mattersievers@gmail.com>
2021-11-07 22:28:03 -0600

    project title and description load on picture

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,9 +5,11 @@ const Project = (prop) => {
     return (
         <section className="project">
                 <div className="container">
-                    <img src={require(`../../assets/pics/${prop.project.name}.png`).default} alt={prop.project.name} ></img>
-                    <a href={prop.project.link} target="_blank" rel="noreferrer" className="websiteLink">website</a>
-                    <a href={prop.project.repo} target="_blank" rel="noreferrer" className="repoLink">github</a>
+                    <img className="projectImg" src={require(`../../assets/pics/${prop.project.name}.png`).default} alt={prop.project.name} ></img>
+                    <h1 className="projectTitle disappearingProject">{prop.project.name}</h1>
+                    <a href={prop.project.link} target="_blank" rel="noreferrer" className="websiteLink disappearingProject">website</a>
+                    <a href={prop.project.repo} target="_blank" rel="noreferrer" className="repoLink disappearingProject">github</a>
+                    <p className="describeProject disappearingProject">{prop.project.description}</p>
                 </div>    
         </section>
     )
