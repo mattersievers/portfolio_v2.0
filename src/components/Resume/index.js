c8546df43538868503a05205435326ea798ce164
Matthew Sievers <mattersievers@gmail.com>
2021-11-14 02:32:48 -0600

    fixed spacing on resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Resume = () => {
     return (
-        <section className="resumeSection d-flex flex-column align-items-center">
+        <section className="resumeSection d-flex flex-column align-items-center justify-content-center">
             <a href="https://drive.google.com/file/d/1mQV6_A6ElUt6uPYaEiEOxWC45VE1semS/view?usp=sharing" className="resumeLink ">Full Resume PDF</a>
-            <ul className="proficiencyList d-flex flex-column justify-content-between flex-wrap align-items-center">
+            <ul className="proficiencyList d-flex flex-wrap align-items-center col-8 justify-content-between">
                 <li>MongoDB/Mongoose/NoSQL</li>
                 <li>SQL/Sequelize</li>
                 <li>Express/React/Node.js</li>
