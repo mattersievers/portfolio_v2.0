6e20b53a019a2df07595b21623dee674d2dc04e5
Matthew Sievers <mattersievers@gmail.com>
2025-07-19 02:45:55 -0400

    corrected links to rehosted heroku projects set some aside for later

--- src/utils/projectList.js
+++ src/utils/projectList.js
@@ -1,31 +1,24 @@
 const projects = [
     {
       name:"Lost and Sound",
-      link:"https://lost-and-sound.herokuapp.com/",
+      link:"https://lost-and-sound-647dba9e03a2.herokuapp.com/",
       technology:"MERN/Stripe/Leaflet",
       description:"Track and recover lost musical equipment. Users can create individual accounts where they can document their current musical equipment and report it lost at any time. The homepage loads a list of lost equipment along with filters to help narrow results and get equipment returned to the rightful owner.",
       repo:"https://github.com/mattersievers/lost_and_sound_v2"
     },
     {
       name:"Budget Tracker",
-      link:"https://tranquil-dawn-49463.herokuapp.com/",
+      link:"https://budget-tracker-v1-5baa2927d7f5.herokuapp.com/",
       technology:"IndexedDb/Cache Storage",
       description:"Budget Tracker allows users to keep track of personal expenses. Add or subtract funds from your account by providing a name and amount for each transaction. This app uses IndexedDB and cache storage methods so that it may be used with limited or no internet connectivity and will update when the user returns online.",
       repo:"https://github.com/mattersievers/budget-tracker"
     },
     {
       name:"gripeSUPPORT",
-      link:"https://gripe-support.herokuapp.com/",
+      link:"https://gripe-support-5241aa86bedb.herokuapp.com/",
       technology:"SQL/Express/Handlebars",
       description:"Post a gripe. Post an encouragement. Users can create an account with a unique username and password to make anonymous posts classified as either a gripe or an encouragement. Users can view and interact with existing posts by giving a word of encouragement or digital hug.",
       repo:"https://github.com/wsbousman/Gripe-Support"
-    },
-    {
-      name:"Shop Shop",
-      link:"https://serene-caverns-16386.herokuapp.com/",
-      technology:"MERN/GraphQL/Stripe",
-      description:"Wares for sale! A full e-commerce website. This MERN stack digital storefront contains many items for sale which can be sorted by category. Users can add multiple items to their digital shopping cart and log in to checkout with a Stipe checkout system (in Test mode).",
-      repo:"https://github.com/mattersievers/shop-shop"
     },
     {
       name:"Soundtracker",
@@ -35,19 +28,27 @@ const projects = [
       repo:"https://github.com/itsmeadriana/Soundtracker"
     },
     {
+      name:"Tech Blog",
+      link:"https://tech-blog-01-61570aa189ec.herokuapp.com/",
+      technology:"SQL/Express/Handlebars",
+      description:"Create a profile with a unique username and password and start blogging today about the latest developments in tech! Write an original blog or comment on an existing blog! It's all up to you!",
+      repo:"https://github.com/mattersievers/tech_blog"
+    }
+  ]
+/*
+{
+      name:"Shop Shop",
+      link:"https://serene-caverns-16386.herokuapp.com/",
+      technology:"MERN/GraphQL/Stripe",
+      description:"Wares for sale! A full e-commerce website. This MERN stack digital storefront contains many items for sale which can be sorted by category. Users can add multiple items to their digital shopping cart and log in to checkout with a Stipe checkout system (in Test mode).",
+      repo:"https://github.com/mattersievers/shop-shop"
+    },
+    {
       name:"Book Search",
       link:"https://book-search-92689.herokuapp.com/",
       technology:"MERN/API",
       description:"Search for books through Google Books API and create an account to save the books to a personalized list.",
       repo:"https://github.com/mattersievers/book_search"
     },
-    {
-      name:"Tech Blog",
-      link:"https://sleepy-earth-77165.herokuapp.com/",
-      technology:"SQL/Express/Handlebars",
-      description:"Create a profile with a unique username and password and start blogging today about the latest developments in tech! Write an original blog or comment on an existing blog! It's all up to you!",
-      repo:"https://github.com/mattersievers/tech_blog"
-    }
-  ]
-
+ */
   module.exports = projects;
